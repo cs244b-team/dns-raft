23b3000e40c416d5ccaa1b5493843f757abb87ae
jrmkim50 <jrmkim50@gmail.com>
2024-06-04 15:43:01 -0700

    This commit implements write latency testing

--- dns.py
+++ dns.py
@@ -6,6 +6,23 @@ def send_one_dns_update_to_random_NS_server(cluster, ip_str):
     server, port = random.choice(cluster)
     subprocess.call(["go", "run", "cmd/client/main.go", "-domain", "www.example.com.", "-server", f"{server}:{port}", "-oneupdate", ip_str])
 
+def write_dns_record(cluster, domain, ip_str): # latency reported in seconds
+    # Update DOMAIN to be at IP_STR
+    server_port = random.choice(cluster)
+    server, port = server_port
+    start_time = time.time()
+    x = subprocess.run(["cmd/client/client", "-domain", domain, "-server", f"{server}:{port}", "-oneupdate", ip_str], capture_output=True)
+    end_time = time.time()
+    x.stderr = x.stderr.decode("utf-8")
+    x.stdout = x.stdout.decode("utf-8")
+    success_message = f"Successfully updated {domain} to {ip_str}"
+    if success_message not in x.stderr:
+        print(x.stderr)
+        elapsed_time = float('inf')
+    else:
+        elapsed_time = end_time - start_time
+    return elapsed_time
+
 def get_dns_record(cluster): # latency reported in seconds
     server_port = random.choice(cluster)
     server, port = server_port
--- latency-testing.py
+++ latency-testing.py
@@ -1,4 +1,7 @@
-cluster = [
+# Latency testing for raft-based DNS
+# Make sure to start the cluster and modify this cluster output before
+# running this script
+dns_cluster = [
     ("127.0.0.1", "7000"),
     ("127.0.0.1", "7001"),
     ("127.0.0.1", "7002"),
@@ -12,28 +15,46 @@ import time
 import dns as _dns
 import numpy as np
 
-### Latencies for reads
-latencies = {}
-throughputs = [1000, 10000, 50000, 100000]
-for throughput in throughputs: # THROUGHPUT reqs/s
-    concurrency = 1000 # we send CONCURRENCY requests in a batch
-    # We can model the events as arriving with a poisson lambda of THROUGHPUT/CONCURRENCY.
-    # Each event, CONCURRENCY requests are sent, so a total of THROUGHPUT requests will be send each second
-    LAMBDA = throughput/concurrency
-    with Pool(4) as pool:
-        res = []
-        for event in range(throughput // concurrency):
-            multiple_results = [pool.apply_async(_dns.get_dns_record, (cluster,)) for i in range(1000)]
-            res += multiple_results
-            time.sleep(np.random.exponential(1/LAMBDA))
-        latencies[throughput] = [r.get(timeout=5) for r in res]
+if __name__ == '__main__':
+    ### Latencies for reads
+    latencies = {}
+    throughputs = [1000,]
+    for throughput in throughputs: # THROUGHPUT reqs/s
+        concurrency = 1000 # we send CONCURRENCY requests in a batch
+        # We can model the events as arriving with a poisson lambda of THROUGHPUT/CONCURRENCY.
+        # Each event, CONCURRENCY requests are sent, so a total of THROUGHPUT requests will be send each second
+        LAMBDA = throughput/concurrency
+        with Pool(4) as pool:
+            res = []
+            for event in range(throughput // concurrency):
+                multiple_results = [pool.apply_async(_dns.get_dns_record, (dns_cluster,)) for i in range(1000)]
+                res += multiple_results
+                time.sleep(np.random.exponential(1/LAMBDA))
+            latencies[throughput] = [r.get(timeout=5) for r in res]
 
-for throughput in throughputs:
-    print('latency (ms) for throughput: ', np.percentile(latencies[throughput], 99) * 1000)
+    for throughput in throughputs:
+        print('latency (ms) for throughput: ', np.percentile(latencies[throughput], 99) * 1000)
 
-### Latencies for writes (incomplete)
-x = subprocess.run(["go", "run", "cmd/client/main.go", "-domain", "www.example.com.", "-server", 
-                    f"127.0.0.1:7000", "-oneupdate", "10.200.201.9"], capture_output=True)
-x.stderr = x.stderr.decode("utf-8")
-x.stdout = x.stdout.decode("utf-8")
-x
+    ### Latencies for writes
+    import socket
+    import struct
+    def generate_random_ipv4():
+        return socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+
+    latencies = {}
+    throughputs = [1000]
+    for throughput in throughputs: # THROUGHPUT reqs/s
+        concurrency = 1000 # we send CONCURRENCY requests in a batch
+        # We can model the events as arriving with a poisson lambda of THROUGHPUT/CONCURRENCY.
+        # Each event, CONCURRENCY requests are sent, so a total of THROUGHPUT requests will be send each second
+        LAMBDA = throughput/concurrency
+        with Pool(4) as pool:
+            res = []
+            for event in range(throughput // concurrency):
+                multiple_results = [pool.apply_async(_dns.write_dns_record, (dns_cluster, "www.example.com.", generate_random_ipv4())) for i in range(1000)]
+                res += multiple_results
+                time.sleep(np.random.exponential(1/LAMBDA))
+            latencies[throughput] = [r.get(timeout=5) for r in res]
+
+    for throughput in throughputs:
+        print('latency (ms) for throughput: ', np.percentile(latencies[throughput], 99) * 1000)
