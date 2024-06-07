cb51b9192540d8564928ec661a2514c236f500f9
jrmkim50 <jrmkim50@gmail.com>
2024-06-07 02:04:38 -0700

    Revert "This commit adds locks to the peer object, so it can safely reconnect"
    This reverts commit 41f7b28572c03d76806e4eaac1edb7c2252b56eb.

--- raft/peer.go
+++ raft/peer.go
@@ -1,39 +1,20 @@
 package raft
 
-import (
-	"net/rpc"
-	"sync"
-)
+import "net/rpc"
 
 type Peer struct {
 	id      int
 	address Address
 	client  *rpc.Client
-	mu      sync.RWMutex
 }
 
 func NewPeer(id int, address Address) *Peer {
 	return &Peer{id: id, address: address}
 }
 
-// Do not call with any of p's locks held
 func (p *Peer) Connect() error {
-	p.mu.RLock()
-	// Check if we need to connect
-	if p.client != nil {
-		p.mu.RUnlock()
-		return nil
-	}
-	// We need to connect since we have a nil client
-	_addr := p.address.String()
-	_pid := p.id
-	p.mu.RUnlock()
-	client, err := rpc.DialHTTPPath("tcp", _addr, rpcServerPath(_pid))
-	// We set a client, so we take a write lock
-	p.mu.Lock()
-	defer p.mu.Unlock()
+	client, err := rpc.DialHTTPPath("tcp", p.address.String(), rpcServerPath(p.id))
 	if err != nil {
-		p.client = nil
 		return err
 	}
 	p.client = client
@@ -41,7 +22,5 @@ func (p *Peer) Connect() error {
 }
 
 func (p *Peer) Disconnect() {
-	p.mu.Lock()
 	p.client.Close()
-	p.mu.Unlock()
 }
